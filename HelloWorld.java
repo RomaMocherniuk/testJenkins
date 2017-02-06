a38a95f5a4043d58eb56e66d615eb08b2e6fa874
Roman Mocherniuk <rmochetc@training.local>
2017-02-06 12:51:56 +0200

    init

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by rmochetc on 06.02.2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+    }
+}
+
+
